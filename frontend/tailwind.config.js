8f7d13a133b1c60705c8124648f62aab5fa58f98
Tatiana Krasikova <tatiana.krasikova@avono.de>
2026-04-20 11:27:47 +0200

    Added tailwind themes!

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,39 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        // Hier wird die CSS-Variable auf einen Tailwind-Namen gemappt
+        primary: 'var(--color-primary)',
+        text: 'var(--color-text)',
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({addBase}) {
+      addBase({
+        '[data-theme="light"]': {
+          '--color-primary': '#ffffff',
+          '--color-text': '#000000',
+          // Deine Variablen oder Styles hier
+        },
+        '[data-theme="dark"]': {
+          '--color-primary': '#1a1a1a',
+          '--color-text': '#ffffff',
+        },
+        '[data-theme="forest"]': {
+          '--color-primary': '#2A9D8F',
+          '--color-secondary': '#E9C46A',
+          '--color-text': '#264653',
+        },
+        '[data-theme="winter"]': {
+          '--color-primary': 'hsl(45 39% 69%)',
+          '--color-secondary': 'rgb(120 210 50)',
+          '--color-text': 'hsl(54 93% 96%)',
+        },
+      })
+    }),
+  ],
+};
